f11f299bec4b6159abb956f66efd6deeb4914a7b
killian <killian.debuitleir@gmail.com>
2019-01-20 16:20:59 +0000

    Removed client width stuff

--- scripts.js
+++ scripts.js
@@ -54,12 +54,3 @@ function searchTable() {
 		} 
 	}
 }
-
-var width = window.innerWidth
-|| document.documentElement.clientWidth
-|| document.body.clientWidth;
-
-var height = window.innerHeight
-|| document.documentElement.clientHeight
-|| document.body.clientHeight;
-console.log('height:', height, 'width:', width);
